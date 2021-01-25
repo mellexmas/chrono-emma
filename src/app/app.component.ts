3f8bdea35d9b8076253569ba97ebe3d33c37baaf
mellexmas <empauhe@gmail.com>
2021-01-25 17:02:25 +0100

    pleins de truc et un peu de chrono

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chrono-emma';
+
+public isStarted: boolean = false;
+public isPaused: boolean = false;
+public time = 0;
+
+
+public start(): void {
+  this.isStarted = true;
+  setInterval( () => {
+    this.time++;
+  }, 1)
+}
+
+public pause():void {
+  this.isPaused = true;
+}
+
+public  stop(): void {
+  this.isStarted = false;
+}
+
 }
