2adbbd5e5fcb05cff7f11a58f19ceb17c2fa9bbf
mellexmas <empauhe@gmail.com>
2021-01-26 09:34:08 +0100

    le chrono timer simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,46 @@ public isStarted: boolean = false;
 public isPaused: boolean = false;
 public time = 0;
 
+private idInterval : number|null = null;
+
 
 public start(): void {
   this.isStarted = true;
-  setInterval( () => {
-    this.time++;
-  }, 1)
+
+  this.startTimer();
+  
+}
+public  stop(): void {
+  this.isStarted = false;
+  this.stopTimer();
+  this.time = 0; 
 }
 
 public pause():void {
   this.isPaused = true;
+ this.stopTimer();
+ 
 }
 
-public  stop(): void {
-  this.isStarted = false;
+public continue(): void {
+  this.isPaused = false;
+  this.startTimer();
 }
+
+private startTimer(): void {
+  this.idInterval = setInterval( () => {
+    this.time++;
+    //console.log(this.time)
+  }, 1);
+}
+
+private stopTimer(): void {
+  if(this.idInterval !== null){
+    clearInterval(this.idInterval);
+  }
+}
+
+
+
 
 }
