868bc1b2402c3b3a98f83fc88292672cd2d7421f
mellexmas <empauhe@gmail.com>
2021-01-26 14:49:48 +0100

    fin exo ou presque

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TimeItem } from './time-item';
+
 
 @Component({
   selector: 'app-root',
@@ -8,50 +10,76 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'chrono-emma';
 
-public isStarted: boolean = false;
-public isPaused: boolean = false;
-public time = 0;
+  public isStarted: boolean = false;
+  public isPaused: boolean = false;
+  public time = 0;
+  private idInterval: number | null = null;
 
-private idInterval : number|null = null;
+  public result: Array<TimeItem> = [];
+  public note: Array<string> = [];
 
-
-public start(): void {
-  this.isStarted = true;
-
-  this.startTimer();
-  
-}
-public  stop(): void {
-  this.isStarted = false;
-  this.stopTimer();
-  this.time = 0; 
-}
-
-public pause():void {
-  this.isPaused = true;
- this.stopTimer();
- 
-}
-
-public continue(): void {
-  this.isPaused = false;
-  this.startTimer();
-}
-
-private startTimer(): void {
-  this.idInterval = setInterval( () => {
-    this.time++;
-    //console.log(this.time)
-  }, 1);
-}
-
-private stopTimer(): void {
-  if(this.idInterval !== null){
-    clearInterval(this.idInterval);
+  public pause(): void {
+    this.isPaused = true;
+    this.stopTimer();
+    //this.result.push(this.time);
+    this.keep();
+    console.table(this.result)
   }
-}
+  public start(): void {
+    this.isStarted = true;
+    this.isPaused = false;
+    this.startTimer();
+  }
+  public stop(): void {
+    this.isStarted = false;
+    this.stopTimer();
+    this.time = 0;
+    this.result = [];
+  }
+  public continue(): void {
+    this.isPaused = false;
+    this.startTimer();
+  }
+  public keep(): void {
+    this.result.push({
+      time : this.time,
+      note : "",
+    });
+  }
+  private startTimer(): void {
+    this.stopTimer();
+    this.idInterval = setInterval(() => {
+      this.time++;
+      //console.log(this.time)
+    }, 1);
+  }
+  private stopTimer(): void {
+    if (this.idInterval !== null) {
+      clearInterval(this.idInterval);
+      
+    }
+  }
 
+  public removeItem(index: number): void {
+  // if (index !== -1) {
+    if (index >= 0 && index < this.result.length){  
+      this.result.splice(index, 1);
+    }
+  }
 
+  public get tableNotEmpty(): boolean {
+      if (this.result.length > 0) {
+        return true
+      }
+    return false;
+  }
+
+ /* public addText($event: KeybordEvent): void {
+    if ($event.code === 'Enter') {
+      const $input: EventTarget | null = $event.target;
+    
+    }
+  }*/
 
 
 }
---
+++ src/app/time-item.d.ts
@@ -0,0 +1,6 @@
+import { NumberSymbol } from "@angular/common";
+
+export interface TimeItem { //nom pas récup d'un autre endroit, nom explicite
+    time : number;
+    note : string;
+}
